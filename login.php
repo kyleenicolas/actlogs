584e72f21381030f37f69d6a5486eac75ba97775
kyleenicolas <167857373+kyleenicolas@users.noreply.github.com>
2024-12-04 00:13:02 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br><br>
+        <button type="submit">Login</button> <a href="index.php"> log-in</a>
+        <button type="submit">register</button> <a href="register.php"> register</a>
+    </form>
+</body>
+</html>
